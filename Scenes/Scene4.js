9c7fc026e204bfa71ff1e505662b6ee13ecfda69
Lenarqa <lenar.valiev211@gmail.com>
2020-03-31 01:08:52 +0700

    Add level4, add bg player bullet book on the map, add player move function

--- Scenes/Scene4.js
+++ Scenes/Scene4.js
@@ -2,4 +2,42 @@ class Scene4 extends Phaser.Scene{
     constructor(){
         super('Level4');
     }
+
+    preload(){
+        this.load.image('bg', 'assets/img/BgLevel4.png');
+        this.load.image('player', 'assets/img/playerLevel4.png');
+        this.load.image('book', 'assets/img/Book.png');
+        this.load.image('bullet', 'assets/img/bullet.png');
+    }
+
+    create(){
+        var h = this.game.config.height;
+        var w = this.game.config.width;
+        console.log(h,  w);
+
+        this.bg = this.add.image(0,0, 'bg');
+        this.bg.setOrigin(0,0);
+
+        this.player = this.physics.add.sprite(w * 0.05, h * 0.5, 'player');
+        this.player.setCollideWorldBounds(true);//player cant run from map
+
+        this.book = this.add.image(w* 0.92, h * 0.5, 'book');
+        this.bullets = this.physics.add.group({
+            key: 'bullet',
+            repeat: 5,
+            setXY: {x: w * 0.15, y: h * 0.2, stepX: 60, stepY:20}
+        });
+        
+        //this.input.on('pointerdown', this.playerRun.bind(this));
+    }
+
+    update(){
+        if(this.input.activePointer.isDown){
+            this.player.x += 4;
+        }
+    }
+    playerRun(pointer){
+        this.player.x += 200;
+    }
 }
+
